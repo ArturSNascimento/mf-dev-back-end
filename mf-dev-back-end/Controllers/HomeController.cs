245646b5fef17b1b6b47c4f2acc0ddd784c8621b
ArturSNascimento <artur.msn2006@gmail.com>
2026-04-23 14:50:34 -0300

    Adicionar arquivos de projeto.

---
+++ mf-dev-back-end/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using mf_dev_back_end.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace mf_dev_back_end.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
