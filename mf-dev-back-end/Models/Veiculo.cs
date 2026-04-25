9db5eff6429e00f8a956d7bfda7a0b2b389e8db4
ArturSNascimento <artur.msn2006@gmail.com>
2026-04-25 16:42:14 -0300

    CRUD Veículo

--- mf-dev-back-end/Models/Veiculo.cs
+++ mf-dev-back-end/Models/Veiculo.cs
@@ -16,9 +16,11 @@ namespace mf_dev_back_end.Models
         public String Placa { get; set; }
 
         [Required(ErrorMessage = "Obrigatorio informar o ano de fabricação!")]
+        [Display(Name = "Ano de Fabricação" )]
         public int AnoFabricacao { get; set; }
 
         [Required(ErrorMessage = "Obrigatorio informar o ano do modelo!")]
+        [Display(Name = "Ano do Modelo")]
         public int AnoModelo { get; set; }
     }
 }
